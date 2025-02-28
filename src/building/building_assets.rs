34f13f449d0343c8db482dcb753008be7c39edca
slplsswkds <slplsswkds@protonmail.com>
2025-02-28 17:28:13 +0200

    refactoring: load_group_X() function

--- src/building/building_assets.rs
+++ src/building/building_assets.rs
@@ -1,3 +1,4 @@
+use bevy::asset::AssetPath;
 use bevy::ecs::system::SystemParam;
 use bevy::prelude::*;
 
@@ -18,6 +19,21 @@ pub struct BuildingAssetsPack {
     pub name: String,
     pub scene: Handle<Scene>,
     pub _snap_points: Vec<Vec3>,
+}
+
+impl BuildingAssetsPack {
+    pub fn new(
+        bridge: &mut BuildingAssetsInitBridge,
+        name: impl Into<String>,
+        asset_path: AssetPath,
+        snap_points: Vec<Vec3>,
+    ) -> Self {
+        Self {
+            name: name.into(),
+            scene: bridge.asset_server.load(asset_path),
+            _snap_points: snap_points,
+        }
+    }
 }
 
 pub struct BuildingsGroup(pub Vec<BuildingAssetsPack>);
@@ -41,44 +57,13 @@ pub struct BuildingAssetsInitBridge<'w> {
 }
 
 impl BuildingAssets {
-    pub fn load_all(bridge: BuildingAssetsInitBridge) -> Self {
-        let foundation = BuildingsGroup::empty();
-
-        let beam = BuildingsGroup::empty();
-
-        let floor = BuildingsGroup::empty()
-            .add(BuildingAssetsPack {
-                name: String::from("Floor 2x2"),
-                _snap_points: Vec::new(),
-                scene: bridge
-                    .asset_server
-                    .load(GltfAssetLabel::Scene(0).from_asset("models/floor.gltf")),
-            })
-            .add(BuildingAssetsPack {
-                name: String::from("Floor 2x2"),
-                _snap_points: Vec::new(),
-                scene: bridge
-                    .asset_server
-                    .load(GltfAssetLabel::Scene(1).from_asset("models/floor.gltf")),
-            });
-
-        let wall = BuildingsGroup::empty().add(BuildingAssetsPack {
-            name: String::from("Wall 2x2"),
-            _snap_points: Vec::new(),
-            scene: bridge
-                .asset_server
-                .load(GltfAssetLabel::Scene(0).from_asset("models/wall.gltf")),
-        });
-
-        let gable = BuildingsGroup::empty();
-
-        let roof = BuildingsGroup::empty().add(BuildingAssetsPack {
-            name: String::from("Roof 2x2 45"),
-            _snap_points: Vec::new(),
-            scene: bridge.asset_server.load(
-                GltfAssetLabel::Scene(0).from_asset("Orange Clay Rooftop Tiles 2k/roof.gltf"),
-            ),
-        });
+    pub fn load_all(mut bridge: BuildingAssetsInitBridge) -> Self {
+        let foundation = load_group_foundation(&mut bridge);
+        let gable = load_group_gabble(&mut bridge);
+        let floor = load_group_floor(&mut bridge);
+        let beam = load_group_beam(&mut bridge);
+        let wall = load_group_wall(&mut bridge);
+        let roof = load_group_roof(&mut bridge);
 
         Self {
             foundation,
@@ -89,4 +74,59 @@ impl BuildingAssets {
             roof,
         }
     }
+}
+
+// #[inline]
+#[allow(unused_variables, unused_mut)]
+fn load_group_foundation(bridge: &mut BuildingAssetsInitBridge) -> BuildingsGroup {
+    BuildingsGroup::empty()
+}
+
+#[inline]
+#[allow(unused_variables, unused_mut)]
+fn load_group_beam(mut bridge: &mut BuildingAssetsInitBridge) -> BuildingsGroup {
+    BuildingsGroup::empty()
+}
+
+#[inline]
+fn load_group_floor(mut bridge: &mut BuildingAssetsInitBridge) -> BuildingsGroup {
+    BuildingsGroup::empty()
+        .add(BuildingAssetsPack::new(
+            &mut bridge,
+            "Floor 2x2",
+            GltfAssetLabel::Scene(0).from_asset("models/floor.gltf"),
+            Vec::new(),
+        ))
+        .add(BuildingAssetsPack::new(
+            &mut bridge,
+            "Floor 1x1",
+            GltfAssetLabel::Scene(1).from_asset("models/floor.gltf"),
+            Vec::new(),
+        ))
+}
+
+#[inline]
+fn load_group_wall(mut bridge: &mut BuildingAssetsInitBridge) -> BuildingsGroup {
+    BuildingsGroup::empty().add(BuildingAssetsPack::new(
+        &mut bridge,
+        "Wall 2x2",
+        GltfAssetLabel::Scene(0).from_asset("models/wall.gltf"),
+        Vec::new(),
+    ))
+}
+
+#[inline]
+#[allow(unused_variables, unused_mut)]
+fn load_group_gabble(mut bridge: &mut BuildingAssetsInitBridge) -> BuildingsGroup {
+    BuildingsGroup::empty()
+}
+
+#[inline]
+fn load_group_roof(mut bridge: &mut BuildingAssetsInitBridge) -> BuildingsGroup {
+    BuildingsGroup::empty().add(BuildingAssetsPack::new(
+        &mut bridge,
+        "Roof 2x2 45",
+        GltfAssetLabel::Scene(0).from_asset("Orange Clay Rooftop Tiles 2k/roof.gltf"),
+        Vec::new(),
+    ))
 }
